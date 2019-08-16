d5e6c5019611394e3a430629bddcd691be4b177a
zhangshuangli <zsl_github@sina.com>
2019-08-16 10:44:57 +0800

    test

---
+++ gamelife/src/main/java/com/ghostgame/gamelife/GamelifeApplication.java
@@ -0,0 +1,13 @@
+package com.ghostgame.gamelife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamelifeApplication.class, args);
+    }
+
+}
